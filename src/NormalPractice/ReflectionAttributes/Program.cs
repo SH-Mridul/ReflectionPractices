a7ec7f2f96d2d51a2b0ce52e195432d0d6c49124
mridulhossain <mridulcse8@gmail.com>
2024-12-11 04:29:22 +0600

    An assembly's get all class,properties,field,also get generic type and generic type all things,and today's work is attributes getting and making a customs attribute

---
+++ src/NormalPractice/ReflectionAttributes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.Metadata;
+using System.Reflection;
+
+namespace ReflectionAttributes
+{
+    public class AnyInfo:Attribute
+    {
+        public string Info { get; }
+        public AnyInfo(string info)
+        {
+            Info = info;
+        }
+    }
+
+
+    [AnyInfo("class of a person")]
+    public class Person
+    {
+        [AnyInfo("Person Age")]
+        public int Age { get; set; }
+
+        [AnyInfo("Person Name")]
+        public string Name { get; set; }
+    }
+
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Get type of the class
+            Type type = typeof(Person);
+            var classAttriutes = type.GetCustomAttributes(typeof(AnyInfo), false);
+            foreach (AnyInfo attr in classAttriutes)
+            {
+                Console.WriteLine(attr.Info);
+            }
+
+            // Get Properties
+            var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"Name of Properties:{property.Name}");
+                var attriutes = property.GetCustomAttributes(typeof(AnyInfo));
+                foreach (AnyInfo attriute in attriutes)
+                {
+                    Console.WriteLine(attriute.Info);
+                }
+            }
+        }
+    }
+}
---
+++ src/NormalPractice/ReflectionPractice/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace ReflectionPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly(); //get dll
+            var types = assembly.GetTypes(); //get all types
+            foreach (var type in types)
+            {
+                if (type.IsDefined(typeof(CompilerGeneratedAttribute), false)) //reduce compiler data
+                {
+                    continue;
+                }
+
+                if (type.Name != "Program")
+                {
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine(type.Name); //printing class name
+                    Console.WriteLine("-------------------------------------");
+                    
+                    //getting all fields
+                    var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic| BindingFlags.Instance);
+                    foreach (var field in fields) 
+                    {
+                        if (field.IsDefined(typeof(CompilerGeneratedAttribute), false)) //reduce compiler data
+                        {
+                            continue;
+                        }
+                        Console.WriteLine($"Name:{field.Name.Trim()}|| data types:{field.FieldType.Name}");
+                    }
+
+
+                    //getting all properties
+                    var properties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance); //get all data public private protected and others
+                    foreach (var property in properties) 
+                    {
+
+                        if (property.PropertyType.IsGenericType) //for generic types
+                        {
+                            
+                            var genericArg = property.PropertyType.GetGenericArguments();
+                            var gnType = string.Join(", ", genericArg.Select(t => t.Name));
+                            Console.WriteLine($"Name:{property.Name}||Type:{property.PropertyType.Name.Split('`')[0]} || genArg:{gnType}");
+                            
+                            //get generic type and its properties name
+                            var genericType = property.PropertyType.GetGenericArguments()[0];
+                            var getProp = genericType.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                            Console.WriteLine("------------generic class information-----------------");
+                            Console.WriteLine(genericType.Name);
+                            foreach (var prop in getProp)
+                            {
+                                Console.WriteLine($"Name:{prop.Name}||Data-type:{prop.PropertyType.Name}");
+                            }
+                            Console.WriteLine("------------generic class end-----------------");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Name: {property.Name}|| Type:{property.PropertyType.Name}"); // normal properties
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
